7b14b34f2f93089d6d68d08f53d2196e7b885ee5
Afsara Tasnim <tratina157@gmail.com>
2023-03-31 21:03:30 +0600

    author section done

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Product.css'
 const Product = (props) => {
     // console.log(props.product)
     const {id,Author_name,Blog_title, Blog_images, Author_images,Publish_Date} = props.product;
@@ -7,11 +7,27 @@ const Product = (props) => {
         <div>
             
             <img className='blog-img' src={Blog_images}alt="" srcset="" />
+
+            <div className='outer-div'>
+            <div className='inner-div'>
             <img className='author-img' src={Author_images}alt="" srcset="" />
-            
+            <div>
             <h3>{Author_name}</h3>
             <p>{Publish_Date}</p>
+            </div>
+            </div>
+
+            <div>
+            <p>5 min read</p>
+            <button>bol</button>
+            </div>
+
+            </div>
+            
             <h2>{Blog_title}</h2>
+            <p>#beginners #programming</p>
+            <a href="">Mark as read</a>
+            <hr></hr>
             
         </div>
     );
